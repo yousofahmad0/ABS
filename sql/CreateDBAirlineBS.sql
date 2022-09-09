dc7068b3d3db91dcd13877fc725883020aceb1d8
yousofahmad0 <yousofahmad81@gmail.com>
2022-09-09 20:36:20 +0300

    repushing

--- sql/CreateDBAirlineBS.sql
+++
@@ -1,9 +0,0 @@
-use master
-go
-
-CREATE DATABASE ABS
- ON  PRIMARY 
-( NAME = ABS_dat, FILENAME = N'C:\Program Files\Microsoft SQL Server\MSSQL15.SQLEXPRESS\MSSQL\DATA\ABS.mdf')
- LOG ON 
-( NAME = ABS_log, FILENAME = N'C:\Program Files\Microsoft SQL Server\MSSQL15.SQLEXPRESS\MSSQL\DATA\ABS.ldf')
-GO
